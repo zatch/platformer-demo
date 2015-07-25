27a5709b81c298aa1082b494861cd7822976e41c
Atom Macumber <amacumber@eagleproductivity.com>
2015-07-25 17:35:32 -0400

    Adjust player physics, add drag + in-air acceleration.
    Signed-off-by: Atom Macumber <amacumber@eagleproductivity.com>

--- src/player.js
+++ src/player.js
@@ -17,8 +17,13 @@ define([
         this.body.collideWorldBounds = true;
 
         // Initialize public properites.
-        this.speed = 100;
-        this.jumpAccel = 300;
+        // Fastest possible horizontal movement speed.
+        this.moveSpeed = 300;
+        // Initial jump speed
+        this.jumpSpeed = 400;
+        // The horizontal acceleration that is applied if the player attemps to
+        // move while they're airborne. 
+        this.jumpMoveAccel = 350;
     }
 
     Player.prototype = Object.create(Phaser.Sprite.prototype);
@@ -28,33 +33,55 @@ define([
 
         // Normal jumping
         if(this.body.onFloor()) {
-            this.body.velocity.y = -this.jumpAccel;
+            this.body.velocity.y = -this.jumpSpeed;
         }
 
         // Wall jumping.
         if(this.body.onWall() && this.body.blocked.left) {
-            this.body.velocity.y = -this.jumpAccel;
-            this.body.velocity.x = this.speed;
+            this.body.velocity.y = -this.jumpSpeed;
+            this.body.velocity.x = this.moveSpeed;
         }
 
         if(this.body.onWall() && this.body.blocked.right) {
-            this.body.velocity.y = -this.jumpAccel;
-            this.body.velocity.x = -this.speed;
+            this.body.velocity.y = -this.jumpSpeed;
+            this.body.velocity.x = -this.moveSpeed;
         }
     };
 
     Player.prototype.moveLeft = function () {
         this.frame = 1;
-        this.body.velocity.x = -this.speed; 
+        this.body.drag.set(0);
+
+        if(this.body.onFloor()) {
+            this.body.acceleration.x = 0;
+            this.body.velocity.x = -this.moveSpeed; 
+        } else {
+            this.body.acceleration.x = -this.jumpMoveAccel;
+            this.body.velocity.clamp(-this.moveSpeed, this.moveSpeed);
+        }
     };
 
     Player.prototype.moveRight = function () {
         this.frame = 0;
-        this.body.velocity.x = this.speed;
+        this.body.drag.set(0);
+
+        if(this.body.onFloor()) {
+            this.body.acceleration.x = 0;
+            this.body.velocity.x = this.moveSpeed;
+        } else {
+            this.body.acceleration.x = this.jumpMoveAccel;
+            this.body.velocity.clamp(-this.moveSpeed, this.moveSpeed);
+        }
     };
 
     Player.prototype.stopMoving = function () {
-        if(this.body.onFloor()) this.body.velocity.x = 0;
+        if(this.body.onFloor()) {
+            this.body.drag.set(this.moveSpeed * 10.5);
+            this.body.acceleration.x = 0;
+        } else {
+            this.body.drag.set(0);
+            this.body.acceleration.x = 0;
+        }
     };
 
     return Player;
