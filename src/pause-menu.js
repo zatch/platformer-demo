7d8292bb8964afcb70e28c349741375aefe17fff
Zach Bennett <xslingshot@gmail.com>
2015-12-04 23:21:11 -0500

    Make PauseMenu a mock-up of what I have in mind.
    No functionality implemented yet.
    
    Signed-off-by: Zach Bennett <xslingshot@gmail.com>

--- src/pause-menu.js
+++ src/pause-menu.js
@@ -20,6 +20,17 @@ define([
 		this.background = new Phaser.Sprite(game, 0, 0, 'pause-background');
         this.addChild(this.background);
 		
+		// Temporary mock-up. Needs to be replaced with something real.
+		// Note: background image current has mock-up graphics in it, too.
+		this.standardGuts = new Phaser.Sprite(game, 80, 80, 'stomach-meter', 7);
+		this.standardGutsLabel = new Phaser.Text(game, 145, 100, this.livesString, { font: "bold 16px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" });
+        this.standardGutsLabel.setText("Regular Gut");
+		this.standardGutsCaption = new Phaser.Text(game, 145, 120, this.livesString, { font: "bold 12px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" });
+        this.standardGutsCaption.setText("A pretty standard puke sack.");
+		this.addChild(this.standardGuts);
+		this.addChild(this.standardGutsLabel);
+		this.addChild(this.standardGutsCaption);
+		
     }
 
     PauseMenu.prototype = Object.create(GameGroup.prototype);
